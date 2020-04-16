f25a9605db572e3d400ee51c1433f31177ab6768
Alexandre Russi <alexandrerussijunior@gmail.com>
2020-04-16 11:53:50 -0300

    ajuste sem numpy 2

--- Problema1/nac1_agua.py
+++ Problema1/nac1_agua.py
@@ -25,10 +25,6 @@ plt.ylabel("Pressão (inHg)")
 # plt.title("Ponto de ebulição da água em diferentes pressões barométricas")
 plt.savefig("temperatura_pressao_noregression.png")
 
-# X = np.asarray([ 1994.,  1995.,  1996.,  1997.,  1998.,  1999.])
-# y = np.asarray([1.2, 2.3, 3.4, 4.5, 5.6, 6.7])
-# print(X)
-
 # regressão linear
 linear_reg = linear_model.LinearRegression()
 linear_reg.fit(temp_F, pressure)
